d931e171d86614dc492a93ea19650e4a095cf67d
Lam <1136833974@qq.com>
2022-05-11 15:00:04 +0800

    Update Kuwo.js

--- Kuwo.js
+++ Kuwo.js
@@ -2,15 +2,6 @@
 酷我音乐 解锁会员试听及部分功能
 
 ***************************
-QuantumultX:
-
-[rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
-
-[mitm]
-hostname = vip1.kuwo.cn
-
-***************************
 Surge4 or Loon:
 
 [Script]
